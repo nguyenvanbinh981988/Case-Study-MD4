46b1d32305b54a967e01ecf244ab933a464ffecd
hoangtranduyLONG <duylonghindu@gmail.com~>
2022-08-02 16:53:12 +0700

    long up room

---
+++ src/main/java/com/example/md4_case_study/service/IRoomService.java
@@ -0,0 +1,8 @@
+package com.example.md4_case_study.service;
+
+import com.example.md4_case_study.model.Room;
+
+
+public interface IRoomService extends IGeneralService<Room> {
+//    Iterable<Room> findAllByNameContaining(String name);
+}
